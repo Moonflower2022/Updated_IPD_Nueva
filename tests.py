c79ea850b12f3f1d7660b243e3a85e731c301ea5
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2025-08-26 20:05:06 -0700

    added better tests

--- tests.py
+++ tests.py
@@ -1,8 +1,11 @@
-import unittest
 from ipd_local.simulation import get_scores, pack_functions, play_match, run_simulation
-import marshal
-from utils import suppress_output
+from ipd_local.get_inputs import get_strategy_code_pairs
+from ipd_local.descriptor import get_client, get_response, describe_strategy
+from ipd_local.utils import suppress_output
 
+import unittest
+import marshal
+import json
 
 def cheat(my_moves, other_moves, current_round):
     return True
@@ -10,6 +13,19 @@ def cheat(my_moves, other_moves, current_round):
 
 def cooperate(my_moves, other_moves, current_round):
     return False
+
+test_code = """import random
+
+def a(aoresnt):
+    return 1
+
+import math
+
+def b(arosent, aoresnt, arstoenars: int) -> int:
+    "aiernst"
+
+def c():
+    pass"""
 
 
 class TestSimulation(unittest.TestCase):
@@ -38,7 +54,7 @@ class TestSimulation(unittest.TestCase):
             ),
             get_scores([True] * 150, [False] * 150),
         )
-    
+
     def test_play_match_noise(self):
         self.assertEqual(
             play_match(
@@ -61,6 +77,41 @@ class TestSimulation(unittest.TestCase):
             )
 
         self.assertEqual(simulation_result["cheat"]["cooperate"], (90.0, 0.0))
+
+
+class TestGetStrategyCodePairs(unittest.TestCase):
+    def test_get_strategy_code_pairs(self):
+        self.assertEqual(
+            get_strategy_code_pairs(test_code),
+            {
+                "a": "def a(aoresnt):\n    return 1\n",
+                "b": "def b(arosent, aoresnt, arstoenars: int) -> int:\n    \"aiernst\"\n",
+                "c": "def c():\n    pass",
+            },
+        )
+
+class TestDescriptor(unittest.TestCase):
+    def test_create_completion_paris(self):
+        client = get_client()
+        response = get_response(client, [{"role": "user", "content": "what is the capital of france? answer in only one word. ex: 'what is the captial of china?' answer: 'beijing"}])
+        self.assertIsNotNone(response)
+        # Check for multiple variations of "Paris"
+        response_content = response.strip()
+        self.assertIn(response_content.lower(), ["paris", "paris.", "paris!"])
+
+    def test_describe_strategy_is_json(self):
+        response = describe_strategy(True, """def cheat():
+            return True""")
+        self.assertIsNotNone(response)
+        self.assertIsInstance(json.loads(response), dict)
+
+    def test_describe_strategy_is_json_10_times(self):
+        for i in range(10):
+            response = describe_strategy(True, f"""def cheat{i}():
+                return True""")
+            self.assertIsNotNone(response)
+            self.assertIsInstance(json.loads(response), dict)
+
 
 
 if __name__ == "__main__":
